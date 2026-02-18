13d1c06c8b0ff7802f087604badebb8fde474883
Lécroart Antoine <115992298+LecroartAntoine@users.noreply.github.com>
2026-02-18 08:15:03 +0100

    fixin stouf

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "drizzle-kit";
+// import type { Config } from "drizzle-kit";
 
-export default {
-  schema: "./lib/db.ts",
-  out: "./drizzle",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-  },
-} satisfies Config;
+// export default {
+//   schema: "./lib/db.ts",
+//   out: "./drizzle",
+//   dialect: "postgresql",
+//   dbCredentials: {
+//     url: process.env.DATABASE_URL!,
+//   },
+// } satisfies Config;
